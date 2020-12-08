85822a65b3990e4d055a7a1c09d6ff3dab197d03
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2020-12-08 18:58:35 +0100

    Add test for posting array of Gladiators

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,32 @@ impl Gladiator {
     }
 }
 
+fn gladiator_vector_response(gladiators: &Vec<Gladiator>) -> tide::Result {
+    let mut response = Response::new(StatusCode::Ok);
+    response.set_content_type(mime::JSON);
+    response.set_body(Body::from_json(&gladiators)?);
+    Ok(response)
+}
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
     app.at("/echo/gladiator").post(echo_gladiator);
+    app.at("/echo/gladiators").post(echo_gladiators);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
 
+// curl localhost:8080/echo/gladiator \
+// -d '{ "id": 1, "name": "test", "games_played": 2, "games_won": 3}'
 async fn echo_gladiator(mut req: Request<()>) -> tide::Result {
     let gladiator: Gladiator = req.body_json().await?;
     gladiator.build_response()
+}
+
+// curl localhost:8080/echo/gladiators \
+// -d '[{ "id": 1, "name": "test", "games_played": 2, "games_won": 3}, { "id": 2, "name": "test", "games_played": 2, "games_won": 3}]'
+async fn echo_gladiators(mut req: Request<()>) -> tide::Result {
+    let gladiators: Vec<Gladiator> = req.body_json().await?;
+    gladiator_vector_response(&gladiators)
 }
