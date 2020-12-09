85e306faf3594d0c0aa4cda4ecc3add37e5259d6
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2020-12-09 19:29:02 +0100

    Add state to routes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 mod player;
 mod routes;
+mod state;
+use state::State;
+use player::query_player;
 use routes::{echo_player, echo_players};
+use sqlx::postgres::PgPoolOptions;
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let mut app = tide::new();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgresql://postgres:unsecure_password@localhost/gladiators_player_service")
+        .await?;
+
+    let mut app = tide::with_state(State { pg_pool: pool });
     app.at("/echo/player").post(echo_player);
     app.at("/echo/players").post(echo_players);
     app.listen("127.0.0.1:8080").await?;
--- src/routes.rs
+++ src/routes.rs
@@ -1,16 +1,16 @@
+use crate::state::State;
 use crate::player::{player_vector_response, Player};
-use tide::Request;
 
 // curl localhost:8080/echo/player \
 // -d '{ "id": 1, "player_name": "test", "games_played": 2, "games_won": 3}'
-pub async fn echo_player(mut req: Request<()>) -> tide::Result {
+pub async fn echo_player(mut req: tide::Request<State>) -> tide::Result {
     let gladiator: Player = req.body_json().await?;
     gladiator.build_response()
 }
 
 // curl localhost:8080/echo/players \
 // -d '[{ "id": 1, "player_name": "test", "games_played": 2, "games_won": 3}, { "id": 2, "name": "test", "games_played": 2, "games_won": 3}]'
-pub async fn echo_players(mut req: Request<()>) -> tide::Result {
+pub async fn echo_players(mut req: tide::Request<State>) -> tide::Result {
     let gladiators: Vec<Player> = req.body_json().await?;
     player_vector_response(&gladiators)
 }
