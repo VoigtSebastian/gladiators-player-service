72fe304466f537fab1f953161c44a8d3ce392423
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2020-12-09 18:58:48 +0100

    Add script to fill player table with example data

---
+++ sql/example_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO players (id, player_name, games_played, games_won) VALUES (1, 'name1', 1, 1);
+INSERT INTO players (id, player_name, games_played, games_won) VALUES (2, 'name2', 2, 2);
+INSERT INTO players (id, player_name, games_played, games_won) VALUES (3, 'name3', 3, 3);
+INSERT INTO players (id, player_name, games_played, games_won) VALUES (4, 'name4', 4, 4);
+INSERT INTO players (id, player_name, games_played, games_won) VALUES (5, 'name5', 5, 5);
+INSERT INTO players (id, player_name, games_played, games_won) VALUES (6, 'name6', 6, 6);
