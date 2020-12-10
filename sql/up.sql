fcc312032bc50c9986c83834e43d0ba713e7c423
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2020-12-10 17:13:09 +0100

    Update database schema

--- sql/up.sql
+++ sql/up.sql
@@ -1,7 +1,8 @@
 -- Create players table
 CREATE TABLE players (
   id SERIAL PRIMARY KEY,
-  player_name VARCHAR NOT NULL,
+  player_name VARCHAR(50) NOT NULL,
   games_played INTEGER NOT NULL,
-  games_won INTEGER NOT NULL
-)
+  games_won INTEGER NOT NULL,
+  UNIQUE(player_name)
+);
