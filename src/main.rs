6940da7608c386ceb17cd0a7b33d47d2ce986af8
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2020-12-12 12:33:42 +0100

    Add routes

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,26 @@ mod error;
 mod player;
 mod routes;
 mod state;
-use routes::{echo_player, echo_players, player_lookup, players_lookup};
+use async_std::future::timeout;
+use routes::*;
 use sqlx::postgres::PgPoolOptions;
 use state::State;
+use std::time::Duration;
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let pool = PgPoolOptions::new()
+    let connect = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgresql://postgres:unsecure_password@localhost/gladiators_player_service")
-        .await?;
+        .connect("postgresql://postgres:unsecure_password@localhost/gladiators_player_service");
+    let pool = timeout(Duration::from_secs(5), connect).await??;
 
     let mut app = tide::with_state(State { pg_pool: pool });
     app.at("/echo/player").post(echo_player);
     app.at("/echo/players").post(echo_players);
-    app.at("/player/:id").get(player_lookup);
+    app.at("/player/:id").get(player_lookup_by_id);
+    app.at("/player/name/:name").get(player_lookup_by_name);
+    app.at("/player/won/:name").post(player_won);
+    app.at("/player/played/:name").post(player_played);
     app.at("/players").get(players_lookup);
     app.with(driftwood::DevLogger);
     app.listen("127.0.0.1:8080").await?;
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,6 @@
 use crate::error::{argument_parsing_error, CustomError, ErrorType};
+use crate::player::{player_played_round, query_player_by_id, query_player_by_name, query_players};
 use crate::player::{player_vector_response, Player};
-use crate::player::{query_player_by_id, query_players};
 use crate::state::State;
 
 // curl localhost:8080/echo/player \
@@ -17,8 +17,8 @@ pub async fn echo_players(mut req: tide::Request<State>) -> tide::Result {
     player_vector_response(&gladiators)
 }
 
-// curl localhost:8080/player/4
-pub async fn player_lookup(req: tide::Request<State>) -> tide::Result {
+// curl localhost:8080/player/id/:id
+pub async fn player_lookup_by_id(req: tide::Request<State>) -> tide::Result {
     let id = match req.param("id")?.parse::<i32>() {
         Err(_) => return argument_parsing_error("id", "i32").into(),
         Ok(id) => id,
@@ -34,7 +34,57 @@ pub async fn player_lookup(req: tide::Request<State>) -> tide::Result {
     }
 }
 
+// TODO: Make spaces in names forbidden?
+// curl localhost:8080/player/name/:name
+pub async fn player_lookup_by_name(req: tide::Request<State>) -> tide::Result {
+    let name = match req.param("name")?.parse::<String>() {
+        Err(_) => return argument_parsing_error("name", "String").into(),
+        Ok(name) => name.replace("%20", " "),
+    };
+
+    match query_player_by_name(&req.state().pg_pool, &name).await {
+        Some(player) => player.build_response(),
+        None => CustomError {
+            error_type: ErrorType::PlayerNotFound,
+            message: format!("Could not find a player with name {}", name),
+        }
+        .into(),
+    }
+}
+
 // curl localhost:8080/players
 pub async fn players_lookup(req: tide::Request<State>) -> tide::Result {
     player_vector_response(&query_players(&req.state().pg_pool).await)
+}
+
+pub async fn player_played(req: tide::Request<State>) -> tide::Result {
+    let name = match req.param("name")?.parse::<String>() {
+        Err(_) => return argument_parsing_error("name", "String").into(),
+        Ok(name) => name.replace("%20", " "),
+    };
+
+    match player_played_round(&req.state().pg_pool, &name, false).await {
+        Ok(player) => player.build_response(),
+        Err(_) => CustomError {
+            error_type: ErrorType::PlayerNotFound,
+            message: format!("Could not find a player with name {}", name),
+        }
+        .into(),
+    }
+}
+
+pub async fn player_won(req: tide::Request<State>) -> tide::Result {
+    let name = match req.param("name")?.parse::<String>() {
+        Err(_) => return argument_parsing_error("name", "String").into(),
+        Ok(name) => name.replace("%20", " "),
+    };
+
+    match player_played_round(&req.state().pg_pool, &name, true).await {
+        Ok(player) => player.build_response(),
+        Err(_) => CustomError {
+            error_type: ErrorType::PlayerNotFound,
+            message: format!("Could not find a player with name {}", name),
+        }
+        .into(),
+    }
 }
