4410a48d73c25d0dac9ca6002b4ff7fa69722714
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2020-12-15 19:51:02 +0100

    Add tracing library

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,34 @@ use routes::*;
 use sqlx::postgres::PgPoolOptions;
 use state::State;
 use std::option_env;
+use tide_tracing::TraceMiddleware;
+use tracing::{info, Level};
 
 static DATABASE_UP: &'static str = include_str!("../sql/up.sql");
 
+#[tracing::instrument]
 #[async_std::main]
 async fn main() -> tide::Result<()> {
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("no global subscriber has been set");
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect(option_env!("DATABASE_URL")
-            .unwrap_or("postgresql://postgres:unsecure_password@localhost/gladiators_player_service"))
+        .connect(option_env!("DATABASE_URL").unwrap_or(
+            "postgresql://postgres:unsecure_password@localhost/gladiators_player_service",
+        ))
         .await?;
+    info!("Successfully connected to postgres database");
 
-    sqlx::query(DATABASE_UP)
-        .execute(&pool)
-        .await?;
+    sqlx::query(DATABASE_UP).execute(&pool).await?;
+    info!("Successfully initialized player table");
 
     let mut app = tide::with_state(State::new(pool));
+    app.with(TraceMiddleware::new());
+
     app.at("/echo/player").post(echo_player);
     app.at("/echo/players").post(echo_players);
     app.at("/player/:id").get(player_lookup_by_id);
@@ -31,7 +43,7 @@ async fn main() -> tide::Result<()> {
     app.at("/player/won/:name").post(player_won);
     app.at("/player/played/:name").post(player_played);
     app.at("/players").get(players_lookup);
-    app.with(driftwood::DevLogger);
-    app.listen("0.0.0.0:8080").await?;
+    app.listen("0.0.0.0:5050").await?;
+
     Ok(())
 }
